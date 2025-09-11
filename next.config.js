0fbff3416df05a3df7a75b6b269ca20b19e49c44
Guilherme Volpatto <ufficiotecnico@sopranowater.com>
2025-09-11 14:23:21 +0000

    do i have to deploy on github or firebase

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/Sopranostampi',
+  basePath: isGithubActions ? '/Sopranostampi' : '',
 };
 
 module.exports = nextConfig;
