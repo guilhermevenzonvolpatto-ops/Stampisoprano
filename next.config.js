651268b604e40909350176a386b139b85d7a9eea
Guilherme Volpatto <ufficiotecnico@sopranowater.com>
2025-09-11 13:19:33 +0000

    why Could not determine the web framework in use.
    Error: Unable to dete

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Sopranostampi',
-  assetPrefix: '/Sopranostampi/',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
